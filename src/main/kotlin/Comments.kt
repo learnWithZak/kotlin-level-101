3ca5196133cead265cb3c61c13b6cf2f6e8fce4e
learnWithZak <learn.with.zak@gmail.com>
2022-08-20 16:19:15 +0200

    refactor: add comments

---
+++ src/main/kotlin/Comments.kt
@@ -0,0 +1,34 @@
+fun main() {
+    // il s'agit d'un commentaire
+    // commentaire 2
+    // commentaire 3
+
+    /*
+    commentaire 1
+    commentaire 2
+    commentaire 3
+    commentaire 4
+     */
+
+    /**
+     * commentaire 1
+     * commentaire 2
+     * commentaire 3
+     * commentaire 4
+     * commentaire 5
+     */
+
+    /**
+     * ligne de commentaires
+     * ligne de commentaires
+     * ligne de commentaires
+     * ligne de commentaires
+     */
+
+    /*
+    /*
+    commentaire 1
+     */
+     commentaire 2
+     */
+}
