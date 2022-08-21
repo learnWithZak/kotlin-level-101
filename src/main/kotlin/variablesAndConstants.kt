032b654e19b7e1b6f5fae5bc4f3b7d0f2170c93e
learnWithZak <learn.with.zak@gmail.com>
2022-08-21 15:33:43 +0200

    refactor: variables

--- src/main/kotlin/variablesAndConstants.kt
+++ src/main/kotlin/variablesAndConstants.kt
@@ -3,4 +3,8 @@ const val compileTimeConstant: Int = 70
 fun main() {
     val number: Int = 10
     val pi: Double = 3.1415
+
+    var amount: Double = 1500.00
+    amount = 2000.00
+    amount = 200_000_000.00
 }
