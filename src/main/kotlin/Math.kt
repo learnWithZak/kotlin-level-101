cd327a6252f2c18cc59892cddcd3055134227da4
learnWithZak <learn.with.zak@gmail.com>
2022-08-21 07:37:51 +0200

    refactor: kotlin math standard library

---
+++ src/main/kotlin/Math.kt
@@ -0,0 +1,9 @@
+import kotlin.math.*
+
+fun main() {
+    println(sin(45 * PI / 180))
+    println(cos(135 * PI / 180))
+    println(sqrt(2.0))
+    println(max(5, 10))
+    println(min(-5, -10))
+}
