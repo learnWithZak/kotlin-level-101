c92693dd7586bd1bfaa0fc60a68c75384379381f
learnWithZak <learn.with.zak@gmail.com>
2022-08-25 05:20:12 +0200

    refactor: numbers and types

---
+++ src/main/kotlin/typesAndOperations/NumberTypes.kt
@@ -0,0 +1,21 @@
+package typesAndOperations
+
+fun main() {
+    /**
+     * For whole numbers, you can use Byte, Short, Int, Long.
+     *      Byte: 8bits.
+     *      Short: 2Bytes = 16bits.
+     *      Int: 4Bytes = 32bits.
+     *      Long: 8Bytes = 64bits.
+     *
+     * For fractional numbers, you can use Double or Float.
+     * Float is less precise than Double, and requires half the storage of Double.
+     */
+
+    val a: Short = 12
+    val b: Byte = 120
+    val c: Int = 10000
+    val answer = a + b + c // int
+    val d: Long = 200
+    val answer2 = a + b + c + d // long
+}
