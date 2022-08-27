c77447c019bf3f158d95a6770e7ae65449ebe1e7
learnWithZak <learn.with.zak@gmail.com>
2022-08-27 06:42:40 +0200

    refactor: Any, Unit, and Nothing

---
+++ src/main/kotlin/typesAndOperations/AnyUnitNothing.kt
@@ -0,0 +1,20 @@
+package typesAndOperations
+
+fun main() {
+    // Any is like Object in Java
+    val anyNumber: Any = 42
+    val anyString: Any = "42"
+
+    //Unit is similar to void in Java. It's a return type
+    fun add(): Unit {
+        val addition = 2 + 2
+        println(addition)
+    }
+
+    // "Nothing" does nothing and never completes
+    fun doNothingForever(): Nothing {
+        while (true) {
+
+        }
+    }
+}
