0f471046761f1823882d8ae91f249b488326ef8a
learnWithZak <learn.with.zak@gmail.com>
2022-08-27 06:48:38 +0200

    refactor: challenge

---
+++ src/main/kotlin/typesAndOperations/Challenge.kt
@@ -0,0 +1,17 @@
+package typesAndOperations
+
+import kotlin.math.PI
+
+fun main() {
+    val coordinates = 2 to 3
+    val (row, column) = coordinates
+
+    val a = 4
+    val b: Short = 100
+    val c: Byte = 12
+    val result = a + b - c // Int
+    println(result)
+
+    println(PI)
+    println(PI.toFloat())
+}
