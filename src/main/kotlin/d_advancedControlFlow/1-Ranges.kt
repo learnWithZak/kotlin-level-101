9037b1e9226f11e9907088dbc29119d41021c4bf
learnWithZak <learn.with.zak@gmail.com>
2022-08-31 04:07:19 +0200

    refactor: ranges

---
+++ src/main/kotlin/d_advancedControlFlow/1-Ranges.kt
@@ -0,0 +1,15 @@
+package d_advancedControlFlow
+
+fun main() {
+    /**
+     * closed ranges
+     */
+    val closedRange = 0..5
+
+    /**
+     * Half-opened ranges
+     */
+    val halfOpenedRange = 0 until 5
+
+    val decreasingRange = 5 downTo 0
+}
