b3022e3a1042ce77d5c5da571288bac13b62f3fc
learnWithZak <learn.with.zak@gmail.com>
2022-09-08 09:18:00 +0200

    refactor: iterating over collections with lambdas

---
+++ src/main/kotlin/l_lambdas/3-IteratingOverCollectionsWithLambdas.kt
@@ -0,0 +1,57 @@
+package l_lambdas
+
+fun main() {
+
+    val values = listOf(1,2,3,4,5,6)
+    values.forEach {
+        println("$it : ${it * it}")
+    }
+
+    var prices = listOf(1.5, 10.0, 4.99, 2.30, 8.19)
+    val largePrices = prices.filter {
+        it > 5.0
+    }
+    println(largePrices.joinToString())
+
+    val salePrices = prices.map {
+        it * 0.9
+    }
+    println(salePrices)
+
+    val userInput = listOf("0", "11", "haha", "42")
+    val numbers = userInput.map {
+        it.toIntOrNull()
+    }
+    println(numbers)
+    val numbers2 = userInput.mapNotNull {
+        it.toIntOrNull()
+    }
+    println(numbers2)
+
+    val data = listOf(1,2,2,2)
+    var sum = data.fold(initial = 0.0) { a, b ->
+        println("a = $a")
+        println("b = $b")
+        a + b
+    }
+    println(sum)
+
+    // reduce uses the 1st element as the starting value
+    sum = data.reduce {acc, d ->
+        acc + d
+    }.toDouble()
+    println(sum)
+
+    val stock = mapOf(
+        1.5 to 5,
+        10.0 to 2,
+        4.99 to 20,
+        2.30 to 5,
+        8.19 to 30
+    )
+    var stockSum = 0.0
+    stock.forEach {
+        stockSum += it.key * it.value
+    }
+    println(stockSum)
+}
