2c25eddb14013d59ae3448dbb2a835209c22a4e2
learnWithZak <learn.with.zak@gmail.com>
2022-09-15 05:15:25 +0200

    refactor: running hierarchy checks

--- src/main/kotlin/q_advancedClasses/1-Inheritance.kt
+++ src/main/kotlin/q_advancedClasses/1-Inheritance.kt
@@ -51,6 +51,38 @@ fun main() {
     /**
      * Runtime hierarchy checks
      */
+
+    var hallMonitor = Student(firstName = "Jill", lastName = "Bananapeel")
+    hallMonitor = oboePlayer
+    println(hallMonitor is OboePlayer)
+    println(hallMonitor !is OboePlayer)
+    println(hallMonitor is Person)
+
+    // (oboePlayer as Student).minimumPracticeTime
+    (hallMonitor as? BandMember)?.minimumPracticeTime
+
+    fun afterClassActivity(student: Student): String {
+        return "Goes home!"
+    }
+
+    fun afterClassActivity(student: BandMember): String {
+        return "Goes to practice!"
+    }
+
+    println(afterClassActivity(oboePlayer)) // practice
+    println(afterClassActivity(oboePlayer as Student)) // home
+
+    val math = Grade(letter = 'B', points = 9.0, credits = 3.0)
+    val science = Grade(letter = 'F', points = 9.0, credits = 3.0)
+    val physics = Grade(letter = 'F', points = 9.0, credits = 3.0)
+    val chemistry = Grade(letter = 'F', points = 9.0, credits = 3.0)
+
+    val dom = StudentAthlete(firstName = "Dom", lastName = "Grady")
+    dom.recordGrade(math)
+    dom.recordGrade(science)
+    dom.recordGrade(physics)
+    dom.recordGrade(chemistry)
+    println(dom.isEligible)
 }
 
 open class BandMember(
@@ -74,4 +106,34 @@ class OboePlayer(
 
 fun phoneBookName(person: Person): String {
     return "${person.lastName}, ${person.firstName}"
+}
+
+class StudentAthlete(
+    firstName: String,
+    lastName: String
+): Student(firstName, lastName) {
+    val failedClasses = mutableListOf<Grade>()
+
+    override fun recordGrade(grade: Grade) {
+        super.recordGrade(grade)
+
+        if (grade.letter == 'F') {
+            failedClasses.add(grade)
+        }
+    }
+
+    val isEligible: Boolean
+        get() = failedClasses.size < 3
+}
+
+class FinalStudent(firstName: String, lastName: String): Person(firstName, lastName)
+// class FinalStudentAthlete(firstName: String, lastName: String): FinalStudent(firstName, lastName) // Compile error
+open class AnotherStudent(firstName: String, lastName: String): Person(firstName, lastName) {
+    open fun recordGrade(grade: Grade) {}
+    fun recordTardy() {}
+}
+
+class AnotherStudentAthlete(firstName: String, lastName: String): AnotherStudent(firstName, lastName) {
+    override fun recordGrade(grade: Grade) {}
+    //override fun recordTardy() {} // error! recordTardy is final
 }
