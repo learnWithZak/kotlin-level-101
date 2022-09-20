87a78f194c9e42aad919bd87121566f491653ace
learnWithZak <learn.with.zak@gmail.com>
2022-09-20 04:41:52 +0200

    refactor: introduction to Interface

---
+++ src/main/kotlin/s_interfaces/Main.kt
@@ -0,0 +1,98 @@
+package s_interfaces
+
+interface Vehicle {
+    fun accelerate()
+    fun stop()
+}
+
+class Unicycle: Vehicle {
+    var paddling = false
+
+    override fun accelerate() {
+        paddling = true
+    }
+
+    override fun stop() {
+        paddling = false
+    }
+
+}
+
+enum class Direction {
+    LEFT, RIGHT
+}
+
+interface DirectionalVehicle {
+    fun accelerate()
+    fun stop()
+    fun turn(direction: Direction)
+    fun description(): String
+}
+
+interface OptionalDirectionalVehicle {
+    fun turn(direction: Direction = Direction.LEFT)
+}
+
+class OptionalDirection: OptionalDirectionalVehicle {
+    override fun turn(direction: Direction) {
+        println(direction)
+    }
+}
+
+interface SpaceVehicle {
+    fun accelerate()
+    fun stop() {
+        println("Whoa, slow down")
+    }
+}
+
+class LightFreighter: SpaceVehicle {
+    override fun accelerate() {
+        println("Proceed to hyperspace!")
+    }
+}
+
+class Starship: SpaceVehicle {
+    override fun accelerate() {
+        println("Warp factor 9 please")
+    }
+
+    override fun stop() {
+        super.stop()
+        println("That kind of hurt!")
+    }
+}
+
+interface VehicleProperties {
+    val weight: Int
+    val name: String
+        get() = "Vehicle"
+
+}
+
+class Car: VehicleProperties {
+    override val weight: Int = 1000
+}
+
+class Tank: VehicleProperties {
+    override val weight: Int
+        get() = 1000
+
+    override val name: String
+        get() = "Tank"
+
+}
+
+fun main() {
+    val car = OptionalDirection()
+    car.turn()
+    car.turn(Direction.RIGHT)
+
+    val freighter = LightFreighter()
+    freighter.accelerate()
+    freighter.stop()
+
+    val starship = Starship()
+    starship.accelerate()
+    starship.stop()
+}
