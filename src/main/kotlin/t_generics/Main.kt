b94fba87f1ec6d2092c1f46c7a5682522dd73965
learnWithZak <learn.with.zak@gmail.com>
2022-09-21 19:48:04 +0200

    refactor: mover class with generics

--- src/main/kotlin/t_generics/Main.kt
+++ src/main/kotlin/t_generics/Main.kt
@@ -5,6 +5,35 @@ fun <T> List<T>.toBulletedList(): String {
     return this.joinToString(prefix = separator, separator = separator, postfix = "\n") { "$it" }
 }
 
+class Mover<T>(thingsToMove: List<T>, val truckHeightInInches: Int = (12 * 12)) {
+    private var thingsLeftInOldPlace = mutableListOf<T>()
+    private var thingsInTrack = mutableListOf<T>()
+    private var thingsInNewPlace = mutableListOf<T>()
+
+    init {
+        thingsLeftInOldPlace.addAll(thingsToMove)
+    }
+
+    fun moveEverythingToTruck() {
+        while (thingsLeftInOldPlace.isNotEmpty()) {
+            val item = thingsLeftInOldPlace.removeAt(0)
+            thingsInTrack.add(item)
+            println("Moved your $item to the truck")
+        }
+    }
+
+    fun moveEverythingIntoNewPlace() {
+        while (thingsInTrack.isNotEmpty()) {
+            val item = thingsInTrack.removeAt(0)
+            thingsInNewPlace.add(item)
+            println("Moved your $item into your new place")
+        }
+    }
+
+    fun finishMove() {
+        println("OK, we finished! We moved to the new place the items:${thingsInNewPlace.toBulletedList()}")
+    }
+}
 fun main() {
     val names = listOf("Bob", "Carol", "Ted", "Alice")
     println("Names: ${names.toBulletedList()}")
