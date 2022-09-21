ba5291e00e9c752604a0b08c21068c21ff678aa7
learnWithZak <learn.with.zak@gmail.com>
2022-09-21 09:01:39 +0200

    refactor: generics in List

--- src/main/kotlin/t_generics/Main.kt
+++ src/main/kotlin/t_generics/Main.kt
@@ -1,8 +1,14 @@
 package t_generics
 
 fun main() {
-    val names: List<String> = listOf("Bob", "Carol", "Ted", "Alice")
+    val names = listOf("Bob", "Carol", "Ted", "Alice")
     println("Names: $names")
     val firstName = names.first()
     println(firstName)
+    // val firstInt: Int = names.first()
+
+    val things = mutableListOf<Any>(1, 2)
+    things.add("Steve")
+    println("Things: $things")
+
 }
