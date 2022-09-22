cf48fed09bbb35f309c3d05258fd8879bcbd2055
learnWithZak <learn.with.zak@gmail.com>
2022-09-22 09:36:40 +0200

    refactor: generic interface

--- src/main/kotlin/t_generics/Main.kt
+++ src/main/kotlin/t_generics/Main.kt
@@ -9,44 +9,92 @@ interface Checkable {
     fun checkIsOk(): Boolean
 }
 
-class Mover<T: Checkable>(thingsToMove: List<T>, val truckHeightInInches: Int = (12 * 12)) {
+class Mover<T: Checkable>(
+    thingsToMove: List<T>,
+    val truckHeightInInches: Int = (12 * 12)
+) {
+
     private var thingsLeftInOldPlace = mutableListOf<T>()
-    private var thingsInTrack = mutableListOf<T>()
+    private var thingsInTruck = mutableListOf<Any>()
     private var thingsInNewPlace = mutableListOf<T>()
+
     private var thingsWhichFailedCheck = mutableListOf<T>()
 
     init {
         thingsLeftInOldPlace.addAll(thingsToMove)
     }
 
-    fun moveEverythingToTruck() {
+    fun moveEverythingToTruck(startingContainer: Container<T>?) {
+        var currentContainer = startingContainer
         while (thingsLeftInOldPlace.isNotEmpty()) {
             val item = thingsLeftInOldPlace.removeAt(0)
+
             if (item.checkIsOk()) {
-                thingsInTrack.add(item)
-                println("Moved your $item to the truck")
+                if (currentContainer != null) {
+                    if (!currentContainer.canAddAnotherItem()) {
+                        moveContainerToTruck(currentContainer)
+                        currentContainer = currentContainer.getAnother()
+                    }
+
+                    currentContainer.addItem(item)
+                    println("Packed your $item!")
+                } else {
+                    thingsInTruck.add(item)
+                    println("Moved your $item to the truck!")
+                }
             } else {
                 thingsWhichFailedCheck.add(item)
-                println("Could not move your $item to the truck")
+                println("Could not move your $item to the truck :[")
+            }
+        }
+
+        currentContainer?.let { moveContainerToTruck(it)}
+    }
+
+    private fun moveContainerToTruck(container: Container<T>) {
+        thingsInTruck.add(container)
+        println("Moved a container with your ${container.contents().toBulletedList()} to the truck!")
+    }
+
+    fun moveEverythingIntoNewPlace() {
+//    val breakableThings = thingsInTruck.filterIsInstance<BreakableThing>()
+//    break
+//    val items = thingsInTruck.filterIsInstance<T>()
+
+        val containers = thingsInTruck.filterIsInstance<Container<T>>()
+        for (container in containers) {
+            thingsInTruck.remove(container)
+            while (container.canRemoveAnotherItem()) {
+                val itemInContainer = container.removeItem()
+                println("Unpacked your $itemInContainer!")
+                tryToMoveItemIntoNewPlace(itemInContainer)
+            }
+        }
+
+        while (thingsInTruck.isNotEmpty()) {
+            @Suppress("UNCHECKED_CAST")
+            val item = thingsInTruck.removeAt(0) as? T
+
+            if (item != null) {
+                tryToMoveItemIntoNewPlace(item)
+            } else {
+                println("Something in the truck was not of the expected generic type: $item")
             }
         }
     }
 
-    fun moveEverythingIntoNewPlace() {
-        while (thingsInTrack.isNotEmpty()) {
-            val item = thingsInTrack.removeAt(0)
-            if (item.checkIsOk()) {
-                thingsInNewPlace.add(item)
-                println("Moved your $item into your new place")
-            } else {
-                thingsWhichFailedCheck.add(item)
-                println("Could not move your $item into your new place :/")
-            }
+    private fun tryToMoveItemIntoNewPlace(item: T) {
+        if (item.checkIsOk()) {
+            thingsInNewPlace.add(item)
+            println("Moved your $item into your new place!")
+        } else {
+            thingsWhichFailedCheck.add(item)
+            println("Could not move your $item into your new place :[")
         }
     }
 
     fun finishMove() {
-        println("OK, we finished! We moved to the new place the items:${thingsInNewPlace.toBulletedList()}")
+        println("OK, we finished! We were able to move your:${thingsInNewPlace.toBulletedList()}")
         if (thingsWhichFailedCheck.isNotEmpty()) {
             println("But we need to talk about your:${thingsWhichFailedCheck.toBulletedList()}")
         }
@@ -78,6 +126,50 @@ class BreakableThing(val name: String, var isBroken: Boolean = false): Checkable
         return name
     }
 }
+
+interface Container<T> {
+    fun canAddAnotherItem(): Boolean
+    fun addItem(item: T)
+
+    fun canRemoveAnotherItem(): Boolean
+    fun removeItem(): T
+
+    fun getAnother(): Container<T>
+    fun contents(): List<T>
+}
+
+class CardboardBox: Container<BreakableThing> {
+
+    private var items = mutableListOf<BreakableThing>()
+
+    override fun canAddAnotherItem(): Boolean {
+        return items.count() < 2
+    }
+
+    override fun canRemoveAnotherItem(): Boolean {
+        return items.isNotEmpty()
+    }
+
+    override fun removeItem(): BreakableThing {
+        val lastItem = items.last()
+        items.remove(lastItem)
+        return lastItem
+    }
+
+    override fun getAnother(): Container<BreakableThing> {
+        return CardboardBox()
+    }
+
+    override fun contents(): List<BreakableThing> {
+        return items.toList()
+    }
+
+    override fun addItem(item: BreakableThing) {
+        items.add(item)
+    }
+
+}
+
 fun main() {
     val names = listOf("Bob", "Carol", "Ted", "Alice")
     println("Names: ${names.toBulletedList()}")
@@ -105,7 +197,7 @@ fun main() {
         CheapThing("Ugly old couch"),
     )
     val cheapMover = Mover(cheapThings)
-    cheapMover.moveEverythingToTruck()
+    cheapMover.moveEverythingToTruck(null)
     cheapMover.moveEverythingIntoNewPlace()
     cheapMover.finishMove()
 
@@ -119,7 +211,7 @@ fun main() {
     )
 
     val expensiveMover = Mover(breakableThings)
-    expensiveMover.moveEverythingToTruck()
+    expensiveMover.moveEverythingToTruck(CardboardBox())
     television.smash()
     expensiveMover.moveEverythingIntoNewPlace()
     expensiveMover.finishMove()
