d71fe8edfa0f14fdee47249e533ae91dc2c03ab7
learnWithZak <learn.with.zak@gmail.com>
2022-09-22 05:17:27 +0200

    refactor: Creating my own generic constraints

--- src/main/kotlin/t_generics/Main.kt
+++ src/main/kotlin/t_generics/Main.kt
@@ -5,10 +5,15 @@ fun <T> List<T>.toBulletedList(): String {
     return this.joinToString(prefix = separator, separator = separator, postfix = "\n") { "$it" }
 }
 
-class Mover<T>(thingsToMove: List<T>, val truckHeightInInches: Int = (12 * 12)) {
+interface Checkable {
+    fun checkIsOk(): Boolean
+}
+
+class Mover<T: Checkable>(thingsToMove: List<T>, val truckHeightInInches: Int = (12 * 12)) {
     private var thingsLeftInOldPlace = mutableListOf<T>()
     private var thingsInTrack = mutableListOf<T>()
     private var thingsInNewPlace = mutableListOf<T>()
+    private var thingsWhichFailedCheck = mutableListOf<T>()
 
     init {
         thingsLeftInOldPlace.addAll(thingsToMove)
@@ -17,21 +22,60 @@ class Mover<T>(thingsToMove: List<T>, val truckHeightInInches: Int = (12 * 12)) 
     fun moveEverythingToTruck() {
         while (thingsLeftInOldPlace.isNotEmpty()) {
             val item = thingsLeftInOldPlace.removeAt(0)
-            thingsInTrack.add(item)
-            println("Moved your $item to the truck")
+            if (item.checkIsOk()) {
+                thingsInTrack.add(item)
+                println("Moved your $item to the truck")
+            } else {
+                thingsWhichFailedCheck.add(item)
+                println("Could not move your $item to the truck")
+            }
         }
     }
 
     fun moveEverythingIntoNewPlace() {
         while (thingsInTrack.isNotEmpty()) {
             val item = thingsInTrack.removeAt(0)
-            thingsInNewPlace.add(item)
-            println("Moved your $item into your new place")
+            if (item.checkIsOk()) {
+                thingsInNewPlace.add(item)
+                println("Moved your $item into your new place")
+            } else {
+                thingsWhichFailedCheck.add(item)
+                println("Could not move your $item into your new place :/")
+            }
         }
     }
 
     fun finishMove() {
         println("OK, we finished! We moved to the new place the items:${thingsInNewPlace.toBulletedList()}")
+        if (thingsWhichFailedCheck.isNotEmpty()) {
+            println("But we need to talk about your:${thingsWhichFailedCheck.toBulletedList()}")
+        }
+    }
+}
+
+class CheapThing(val name: String): Checkable {
+
+    override fun checkIsOk(): Boolean {
+        return true
+    }
+
+    override fun toString(): String {
+        return name
+    }
+}
+
+class BreakableThing(val name: String, var isBroken: Boolean = false): Checkable {
+
+    fun smash() {
+        isBroken = true
+    }
+
+    override fun checkIsOk(): Boolean {
+        return !isBroken
+    }
+
+    override fun toString(): String {
+        return name
     }
 }
 fun main() {
@@ -54,4 +98,29 @@ fun main() {
         .filter { it.contains("e") }
         .map { "Value for $it: ${map[it]}" }
     println("Values for keys with E: ${valuesForKeysWithE.toBulletedList()}")
+
+    val cheapThings = listOf(
+        CheapThing("Cinder block table"),
+        CheapThing("Box of old books"),
+        CheapThing("Ugly old couch"),
+    )
+    val cheapMover = Mover(cheapThings)
+    cheapMover.moveEverythingToTruck()
+    cheapMover.moveEverythingIntoNewPlace()
+    cheapMover.finishMove()
+
+    println("-----------")
+
+    val television = BreakableThing("Flat screen television")
+    val breakableThings = listOf(
+        television,
+        BreakableThing("Mirror"),
+        BreakableThing("Guitar")
+    )
+
+    val expensiveMover = Mover(breakableThings)
+    expensiveMover.moveEverythingToTruck()
+    television.smash()
+    expensiveMover.moveEverythingIntoNewPlace()
+    expensiveMover.finishMove()
 }
